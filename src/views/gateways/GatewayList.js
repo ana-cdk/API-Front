09aa79310d7558de428ce796ebee4eeaa3c66080
♥Ana Carolina♥ <anadielkotz@gmail.com>
2024-06-04 11:28:12 -0300

    update: fix auth and list gateways per user

--- src/views/gateways/GatewayList.js
+++ src/views/gateways/GatewayList.js
@@ -9,27 +9,29 @@ function GatewayList() {
 
     useEffect(() => {
         const token = localStorage.getItem('jwtToken');
-        fetch(`${URL_API}gateway`, {
+        const userId = localStorage.getItem('userId');
+        
+        fetch(`${URL_API}gateway/user/${userId}`, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else if (response.status === 401) {
-                    navigate('/auth'); // Redireciona para a página de login
-                    throw new Error('Unauthorized');
-                } else {
-                    throw new Error('Unexpected error');
-                }
-            })
-            .then(res => {
-                setGateways(res);
-            })
-            .catch(e => console.log(e));
-    }, []);
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else if (response.status === 401) {
+                navigate('/auth'); // Redireciona para a página de login
+                throw new Error('Unauthorized');
+            } else {
+                throw new Error('Unexpected error');
+            }
+        })
+        .then(res => {
+            setGateways(res);
+        })
+        .catch(e => console.log(e));
+    }, [navigate]);
 
     function handleEdit(id) {
         navigate(`/gateway/${id}`);
@@ -44,14 +46,14 @@ function GatewayList() {
                     'Authorization': `Bearer ${token}`
                 }
             })
-                .then(response => {
-                    if (response.ok) {
-                        setGateways(prevGateways => prevGateways.filter(gateway => gateway.idGateway !== id));
-                    } else {
-                        alert('Erro ao deletar o gateway');
-                    }
-                })
-                .catch(e => console.log(e));
+            .then(response => {
+                if (response.ok) {
+                    setGateways(prevGateways => prevGateways.filter(gateway => gateway.idGateway !== id));
+                } else {
+                    alert('Erro ao deletar o gateway');
+                }
+            })
+            .catch(e => console.log(e));
         }
     }
 
@@ -61,68 +63,51 @@ function GatewayList() {
 
     return (
         <>
-            
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 
             <div className={`row ${styles.marginBottom}`}>
-                <div class="col-sm-1"></div>
-                    <div class="col-sm-10">
-                        <div class="card">
-                            <div class="card-body">
-                                <div className={styles.headerRow}>
-                                    <h3>Gateways</h3>
-                                    <i type="button" className="fa-solid fa-plus" onClick={handleAddNewGateway}></i>
-                                </div>
-
-                                <table class="table table-striped">
-                                    <thead class="table-dark">
-                                        <tr>
-                                            <th>Nome</th>
-                                            <th>Descrição</th>
-                                            <th>Endereço</th>
-                                            <th>Dispositivos</th>
-                                            <th>Ações</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {gateways.map(gateway => (
-                                            <tr key={gateway.idGateway}>
-                                                <td>{gateway.nome}</td>
-                                                <td>{gateway.descricao}</td>
-                                                <td>{gateway.endereco}</td>
-                                                <td>
-                                                    {gateway.dispositivos?.map(dispositivo => (
-                                                        <div key={dispositivo.idDispositivo}>{dispositivo.nome}</div>
-                                                    ))}
-                                                </td>
-                                                <td className={styles.btnGap}>
-                                                    <i type="button" className={`fa-solid fa-pen-to-square ${styles.editIcon}`} onClick={() => handleEdit(gateway.idGateway)}></i>
-                                                    <i type="button" className={`fa fa-trash ${styles.deleteIcon}`} onClick={() => handleDelete(gateway.idGateway)}></i>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-
-                                <table className="table table-striped">
-                                    <thead>
-                                        <tr>
-                                            
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        
-                                    </tbody>
-                                </table> 
+                <div className="col-sm-1"></div>
+                <div className="col-sm-10">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className={styles.headerRow}>
+                                <h3>Gateways</h3>
+                                <i type="button" className="fa-solid fa-plus" onClick={handleAddNewGateway}></i>
                             </div>
+
+                            <table className="table table-striped">
+                                <thead className="table-dark">
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Descrição</th>
+                                        <th>Endereço</th>
+                                        <th>Dispositivos</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {gateways.map(gateway => (
+                                        <tr key={gateway.idGateway}>
+                                            <td>{gateway.nome}</td>
+                                            <td>{gateway.descricao}</td>
+                                            <td>{gateway.endereco}</td>
+                                            <td>
+                                                {gateway.dispositivos?.map(dispositivo => (
+                                                    <div key={dispositivo.idDispositivo}>{dispositivo.nome}</div>
+                                                ))}
+                                            </td>
+                                            <td className={styles.btnGap}>
+                                                <i type="button" className={`fa-solid fa-pen-to-square ${styles.editIcon}`} onClick={() => handleEdit(gateway.idGateway)}></i>
+                                                <i type="button" className={`fa fa-trash ${styles.deleteIcon}`} onClick={() => handleDelete(gateway.idGateway)}></i>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
+                </div>
             </div>
-
-
-
-
-
         </>
     );
 }
